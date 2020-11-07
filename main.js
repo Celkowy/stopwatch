387ba94c7d4f686fab3135e1bfd6fd8a37cf1732
tucelek@gmail.com <tucelek@gmail.com>
2020-11-07 22:09:54 +0100

    changes in logic and visual update

--- main.js
+++ main.js
@@ -1,70 +1,74 @@
-const change = document.querySelector('.change');
-const reset = document.querySelector('.reset')
-const main = document.querySelector('.main')
+const reset = document.querySelector('.reset');
+const main = document.querySelector('.main');
+const min = document.querySelector('.minutes');
+const sec = document.querySelector('.seconds');
+const milisec = document.querySelector('.milliseconds');
 
 
 let time = 0;
 let seconds = 0;
-let minutes = "";
+let minutes = 0;
+let milisecs = 0;
 let active = false;
 let interval;
 
+
+const displayTime = () => {
+  min.textContent = 0;
+  sec.textContent = 0;
+  milisec.textContent = "00";
+}
+
+const minAndSecReset = () => {
+  minutes = 0;
+  seconds = 0;
+}
+const resetStopwatch = () => {
+    minAndSecReset();
+    clearInterval(interval);
+    main.textContent = "Start";
+    displayTime();
+}
+
 const stopwatch = () => {
   time++;
-  if(time == 100) {
+
+    if(time == 100) {
     time = 0;
     seconds++;
   }
+
   if(seconds == 60) {
     seconds = 0;
     minutes++;
   }
 
-  if(minutes == 15){
-    minutes = 0;
-    seconds = 0;
-    clearInterval(interval)
-    main.textContent = "Start"
-    main.className = "main"
-  }
+  min.innerHTML = minutes.toString().bold();
+  sec.innerHTML = seconds.toString().bold();
+  time < 10 ? milisec.textContent = `0${time}` : milisec.textContent = `${time}`;
 
-  if(minutes == 0){
-    if(time < 10) {
-      change.textContent = `${seconds}.0${time}`;
-    } else{
-      change.textContent = `${seconds}.${time}`
-    }
-  } else{
-    if(time < 10){
-      change.textContent = `${minutes}.${seconds}.0${time}`
-    } else if (time < 10 && seconds < 10) {
-      change.textContent = `${minutes}.0${seconds}.0${time}`
-    } else{
-      change.textContent = `${minutes}.${seconds}.${time}`
-    }
-  }
+  minutes == 15 ? resetStopwatch(1) : minutes = 0;
 }
 
 main.addEventListener('click', () => {
   
   if(!active){
-    interval = setInterval(stopwatch, 10)
+    interval = setInterval(stopwatch, 1);
     active = !active;
-    main.textContent = 'Stop'
+    main.textContent = "Stop";
   } 
   else{
-    main.textContent = 'Start'
-    clearInterval(interval)
+    main.textContent = "Start";
+    clearInterval(interval);
     active = !active;
   }
 })
 
 reset.addEventListener('click', () => {
-  clearInterval(interval)
-  minutes = 0;
-  seconds = 0;
-  change.textContent = "---"
-  main.textContent = "Start"
+  clearInterval(interval);
+  minAndSecReset();
+  displayTime();
+  main.textContent = "Start";
   active = false;
 })
 
